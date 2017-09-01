4d4003b2fdcb18c1841ac1193cf30f691002f691
rodasalexa17 <30929316+rodasalexa17@users.noreply.github.com>
2017-09-01 11:09:42 -0400

    Add files via upload

---
+++ ResponseFilter.java
@@ -0,0 +1,52 @@
+package filterPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebFilter("/ResponseFilter")
+public class ResponseFilter implements Filter {
+
+   
+    public ResponseFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// Open the session
+		request.setAttribute("message","welcome");
+		PrintWriter out = response.getWriter();
+		out.print("This is before the request is sent to the servlet");
+		chain.doFilter(request, response);
+		out.print("This is after the servlet sent the response to the client");
+		
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ SourceServlet.java
@@ -0,0 +1,28 @@
+package filterPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SourceServlet
+ */
+//@WebServlet("/SourceServlet")
+public class SourceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String message = (String) request.getAttribute("message");
+		PrintWriter out = response.getWriter();
+		out.print("<body><h2>"+ message+"</h2></body>");
+	}
+
+}
